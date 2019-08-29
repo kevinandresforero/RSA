e44826fd7764df90cbc62c3da409441ce9d43789
Kevin Andres Forero Guaitero <42241664+kevinandresforero@users.noreply.github.com>
2019-08-29 12:31:38 -0500

    Add files via upload

--- RSA.py
+++ RSA.py
@@ -1,8 +1,44 @@
+import  tkinter as tk
+
+'''
+marco = tk.Tk()
+marco.title('Encriptación SRA')
+marco.configure(background='#000000')
+marco.geometry('400x400')
+marco.mainloop()
+'''''
+
 class rca:
-    tex = ("tu y yo <3")
-    _p = 23
-    _q = 31
-    _n = 29
+
+    _tex = (" tu y yo <3 ")
+    _p = int(input(" digite un Primo"))
+    _q = 2
+    _n = 3
+    primo = False
+    a =[_p, _q , _n]
+    aprimo = [False,False,False]
+    for j in range (len(aprimo)):
+        while(aprimo[j] == False):
+            for i in range (len(a)):
+                a[i] = int(input(" digite un Primo"))
+                if(a[i] < 2):
+                    print("no Valido")
+                else:
+                    if (_p == 2 ):
+                        aprimo[j] = True
+                        print(" Es Primo ")
+                    else:
+                        x = 2
+                        while(x < _p):
+                            if(_p % x == 0):
+                                print(" No es Primo ")
+                                break
+                            else:
+                                if(x==_p-1):
+                                        aprimo[j] = True
+                                        print(" Es Primo ")
+                                x = x+1
+
     _fi = (_p-1)*(_q-1)
     _z = _p * _q
     _Cp = _n+1
@@ -13,10 +49,7 @@ class rca:
         _Cp = _Cp + 1
     print(" Cp = "+str(_Cp))
     print(" z = "+str(_z))
-    _lTex = tex
+    _lTex = _tex
 
     for i in range (len(_lTex)):
         print("   "+str(_lTex[i])+"   =   "+str((ord(_lTex[i]) ** _n) % _z))
-
-
-
