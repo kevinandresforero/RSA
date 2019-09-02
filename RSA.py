886770bf2812ec53e96f08704de8c35557b64bdc
Kevin Andres Forero Guaitero <42241664+kevinandresforero@users.noreply.github.com>
2019-09-02 00:05:22 -0500

    Con Interfaz simple sin funcionalidad

--- RSA.py
+++ RSA.py
@@ -1,55 +1,93 @@
 import  tkinter as tk
+from tkinter import Canvas
 
-'''
-marco = tk.Tk()
-marco.title('Encriptación SRA')
-marco.configure(background='#000000')
-marco.geometry('400x400')
-marco.mainloop()
-'''''
+def es_primo(numero_leido):
+
+    verificar= False
+    numero = int(numero_leido)
+    contador = 0
+    verificar= False
+    for i in range(1,numero+1):
+        if (numero% i)==0:
+           contador = contador + 1
+        if contador >= 3:
+            verificar=True
+            break
+
+    if contador==2 or verificar==False:
+        print ("El numero es primo")
+        primo = True
+        return primo
+
+    else:
+        print ("El numero no es primo")
+        primo = False
+        return primo
 
 class rca:
 
-    _tex = (" tu y yo <3 ")
-    _p = int(input(" digite un Primo"))
-    _q = 2
-    _n = 3
+    marco = tk.Tk()
+    marco.title('Encriptación en SRA')
+    marco.configure(background='#000000')
+    marco.geometry('550x300')
+    marco.resizable(False,False)
+
+
+    lTitle = tk.Label(marco, text="Complete los Siguientes Campos", fg="yellow" ,bg ="black").grid(row=0,column=0)
+    lText = tk.Label(marco, text="El Texto a Codificar", fg="yellow" ,bg ="black").grid(row=1,column=1)
+    lp = tk.Label(marco, text="El Primer Número Primo", fg="yellow" ,bg ="black").grid(row=3,column=1)
+    lq = tk.Label(marco, text="El Segundo Número Primo", fg="blue" ,bg ="black").grid(row=4,column=1)
+    ln = tk.Label(marco, text="El Tercer Número Primo", fg="red" ,bg ="black").grid(row=5,column=1)
+
+    cText = tk.Entry(marco).grid(row=1,column=2)
+    cP = tk.Entry(marco).grid(row=3,column=2)
+    cQ = tk.Entry(marco).grid(row=4,column=2)
+    cN = tk.Entry(marco).grid(row=5,column=2)
+
+    codificar = tk.Button(marco, text = "Codificar" , bg = "black" , fg = "blue").grid(row=4,column=0)
+
+    marco.mainloop()
+
+    print(str(cP)+" "+str(cQ)+" "+str(cN))
+
     primo = False
-    a =[_p, _q , _n]
-    aprimo = [False,False,False]
-    for j in range (len(aprimo)):
-        while(aprimo[j] == False):
-            for i in range (len(a)):
-                a[i] = int(input(" digite un Primo"))
-                if(a[i] < 2):
-                    print("no Valido")
-                else:
-                    if (_p == 2 ):
-                        aprimo[j] = True
-                        print(" Es Primo ")
-                    else:
-                        x = 2
-                        while(x < _p):
-                            if(_p % x == 0):
-                                print(" No es Primo ")
-                                break
-                            else:
-                                if(x==_p-1):
-                                        aprimo[j] = True
-                                        print(" Es Primo ")
-                                x = x+1
+
+    while(primo == False):
+        aux = 0
+        _p = int(cP.get())
+        print(cP)
+        if(es_primo(_p) == True):
+            aux = aux + 1
+        _q = int(cQ.get())
+        if(es_primo(_q) == True):
+            aux = aux + 1
+        _n = int(cN.get())
+        if(es_primo(_p) == True):
+            aux = aux +1
+        if(aux >= 3):
+            primo = True
 
     _fi = (_p-1)*(_q-1)
     _z = _p * _q
     _Cp = _n+1
 
+
+    marco.mainloop()
+
     #Generando la clave publica
+
 
     while((_n*_Cp)%_fi != 1):
         _Cp = _Cp + 1
+
+    _lTex = cText.get()
+    encript = []
+    dencript = []
+
     print(" Cp = "+str(_Cp))
     print(" z = "+str(_z))
-    _lTex = _tex
 
     for i in range (len(_lTex)):
-        print("   "+str(_lTex[i])+"   =   "+str((ord(_lTex[i]) ** _n) % _z))
+        encript.append(((ord(_lTex[i])) ** _n) % (_z))
+        dencript.append((encript[i]**_Cp) % _z)
+        print(str(ord(_lTex[i]))+"  :   "+str(encript[i])+" :   "+str((dencript[i])))
